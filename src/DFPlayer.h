5763e5fc54618f2832809c1b8f2523ab0b10388b
enjoyneering <10623649+enjoyneering@users.noreply.github.com>
2021-06-30 18:00:09 -0400

    v1.1.0

--- src/DFPlayer.h
+++ src/DFPlayer.h
@@ -11,7 +11,7 @@
    - 24-bit DAC
    - 90dB output dynamic range
    - SNR over 85dB
-   - micro SD card up to 32GB (FAT16, FAT32)
+   - micro SD card, up to 32GB (FAT16, FAT32)
    - USB-Disk up to 32GB (FAT16, FAT32)
    - supports up to 100 folders, each folder can be assigned to 001..255 songs
    - built-in 3W mono amplifier, MD8002 AB-Class
@@ -56,13 +56,13 @@
 #define DFPLAYER_PLAY_TRACK           0x03 //play tracks in chronological order, by upload time
 #define DFPLAYER_SET_VOL_UP           0x04 //increment volume by 1
 #define DFPLAYER_SET_VOL_DOWN         0x05 //decrement volume by 1
-#define DFPLAYER_SET_VOL              0x06 //volume range 0..30
-#define DFPLAYER_SET_EQ               0x07 //0=Off, 1=Pop, 2=Rock, 3=Jazz, 4=Classic, 5=Bass
+#define DFPLAYER_SET_VOL              0x06 //set volume, range 0..30
+#define DFPLAYER_SET_EQ               0x07 //0=Off, 1=Pop, 2=Rock, 3=Jazz, 4=Classic, 5=Bass (may not be supported by some modules)
 #define DFPLAYER_LOOP_TRACK           0x08 //playing & looping track number 0001..9999
-#define DFPLAYER_SET_PLAY_SRC         0x09 //1=USB-Disc, 2=TF-Card, 3=Aux, 4=???, 5=NOR-Flash, 6=Sleep
+#define DFPLAYER_SET_PLAY_SRC         0x09 //1=USB-Disc, 2=TF-Card, 3=Aux, 4=???, 5=NOR-Flash, 6=Sleep (3..6 may not be supported by some modules)
 #define DFPLAYER_SET_STANDBY_MODE     0x0A //put player in standby mode, not the same as sleep mode
-#define DFPLAYER_SET_NORMAL_MODE      0x0B //pull player out of standby mode, DOES NOT WORK!!!
-#define DFPLAYER_RESET                0x0C //reset all settings to factory default
+#define DFPLAYER_SET_NORMAL_MODE      0x0B //pull player out of standby mode (may not be supported by some modules)
+#define DFPLAYER_RESET                0x0C //reset module, set all settings to factory default
 #define DFPLAYER_RESUME_PLAYBACK      0x0D //resume playing current track
 #define DFPLAYER_PAUSE                0x0E //pause playing current track
 #define DFPLAYER_PLAY_FOLDER          0x0F //play track number 1..255 from folder number 1..99
@@ -70,7 +70,8 @@
 #define DFPLAYER_REPEAT_ALL           0x11 //repeat playback all files in chronological order
 #define DFPLAYER_PLAY_MP3_FOLDER      0x12 //play track number 0001..9999 from "mp3" folder 
 #define DFPLAYER_PLAY_ADVERT_FOLDER   0x13 //interrupt current track & play track number 0001..9999 from "advert" folder, than resume current track
-#define DFPLAYER_STOP_ADVERT_FOLDER   0x15 //stop interrupting current track to play track from "advert" folder
+#define DFPLAYER_PLAY_3000_FOLDER     0x14 //play track number 0001..3000 from folder that supports 3000 tracks (may not be supported by some modules)
+#define DFPLAYER_STOP_ADVERT_FOLDER   0x15 //stop interrupting current track while playing track from "advert" folder
 #define DFPLAYER_STOP_PLAYBACK        0x16 //stop playing current track
 #define DFPLAYER_REPEAT_FOLDER        0x17 //repeat playback folder number 01..99
 #define DFPLAYER_RANDOM_ALL_FILES     0x18 //play all tracks in random order
@@ -80,23 +81,23 @@
 /* request command controls */
 #define DFPLAYER_GET_STATUS           0x42 //get current stutus, see NOTE
 #define DFPLAYER_GET_VOL              0x43 //get current volume, range 0..30
-#define DFPLAYER_GET_EQ               0x44 //get current EQ, 0=Off, 1=Pop, 2=Rock, 3=Jazz, 4=Classic, 5=Bass
-#define DFPLAYER_GET_PLAY_MODE        0x45 //get current loop mode, 0=loop all, 1=loop folder, 2=loop track, 3=random, 4=disable
+#define DFPLAYER_GET_EQ               0x44 //get current EQ, 0=Off, 1=Pop, 2=Rock, 3=Jazz, 4=Classic, 5=Bass (may not be supported by some modules)
+#define DFPLAYER_GET_PLAY_MODE        0x45 //get current loop mode, 0=loop all, 1=loop folder, 2=loop track, 3=random, 4=disable (may not be supported by some modules)
 #define DFPLAYER_GET_VERSION          0x46 //get software version
 #define DFPLAYER_GET_QNT_USB_FILES    0x47 //get total number of tracks on USB-Disk
 #define DFPLAYER_GET_QNT_TF_FILES     0x48 //get total number of tracks on TF-card
-#define DFPLAYER_GET_QNT_FLASH_FILES  0x49 //get total number of tracks on NOR-Flash
+#define DFPLAYER_GET_QNT_FLASH_FILES  0x49 //get total number of tracks on NOR-Flash (may not be supported by some modules)
 #define DFPLAYER_GET_USB_TRACK        0x4B //get currently playing track number on USB-Disk
 #define DFPLAYER_GET_TF_TRACK         0x4C //get currently playing track number on TF-card
-#define DFPLAYER_GET_FLASH_TRACK      0x4D //get currently playing track number on NOR-Flash
+#define DFPLAYER_GET_FLASH_TRACK      0x4D //get currently playing track number on NOR-Flash (may not be supported by some modules)
 #define DFPLAYER_GET_QNT_FOLDER_FILES 0x4E //get total number of tracks in folder
-#define DFPLAYER_GET_QNT_FOLDERS      0x4F //DOES NOT WORK, return 04, second time 07 & stop play!!! get total number of folders in current source
+#define DFPLAYER_GET_QNT_FOLDERS      0x4F //get total number of folders in current source (may not be supported by some modules)
 
 /* module returned codes at the end of any playback operation or if any command error */
 #define DFPLAYER_RETURN_CODE_OK       0x41 //OK, return if command is accepted & ACK/feedback byte is set to 0x01
 #define DFPLAYER_RETURN_ERROR         0x40 //see NOTE
 #define DFPLAYER_RETURN_CODE_DONE     0x3D //track playback is over
-#define DFPLAYER_RETURN_CODE_READY    0x3F //ready after boot or reset, DL-byte current source???
+#define DFPLAYER_RETURN_CODE_READY    0x3F //ready after boot or reset
 
 class DFPlayer
 {
@@ -116,6 +117,7 @@ class DFPlayer
 
    void playFolder(uint8_t folder, uint8_t track);
    void playMP3Folder(uint16_t track);
+   void play3000Folder(uint16_t track);
    void playAdvertFolder(uint16_t track);
    void stopAdvertFolder();
 
@@ -149,6 +151,7 @@ class DFPlayer
    uint16_t getTrackUSB();
    uint16_t getTrackNORFlash();
    uint8_t  getTotalTracksFolder(uint8_t folder);
+   uint8_t  getTotalFolders();
    uint8_t  getCommandStatus();
 
   private:
