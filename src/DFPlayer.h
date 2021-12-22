96865b63be21a4a0ced0688fd837d11de42eb992
enjoyneering <10623649+enjoyneering@users.noreply.github.com>
2021-12-22 14:25:22 -0500

    Update DFPlayer.h

--- src/DFPlayer.h
+++ src/DFPlayer.h
@@ -105,7 +105,7 @@ typedef enum : uint8_t
 {
   DFPLAYER_MINI        = 0x00, //DFPlayer Mini, MP3-TF-16P, FN-M16P (YX5200, YX5300, JL AA20HF)
   DFPLAYER_FN_X10P     = 0x01, //FN-M10P, FN-S10P (FN6100)
-  DFPLAYER_HW_247A     = 0x02, //DFPlayer Mini HW-247A, MP3-TF-16P V3.0 (GD3200B, MH2024K)
+  DFPLAYER_HW_247A     = 0x02, //DFPlayer Mini HW-247A (GD3200B)
   DFPLAYER_NO_CHECKSUM = 0x03  //no checksum calculation, not recomended for MCU without external crystal oscillator
 }
 DFPLAYER_MODULE_TYPE;
